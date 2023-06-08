37204912ed2b73ff21a8eea41e0101c71d369671
lucaslagant <lucaslagant@gmail.com>
2023-06-08 16:36:26 +0200

    barre de recherche fonctionnel

--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
+import {useState, useEffect} from 'react';
+import { Box, Typography} from '@mui/material';
+import {Video} from './';
+import { fetchFromAPI } from '../utils/fetchFromAPI';
+import { useParams } from 'react-router-dom';
 
 const SearchFeed = () => {
+
+  const [videos, setVideos] = useState([]);
+  const { searchTerm } = useParams();
+
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=${searchTerm}`)
+      .then((data) => setVideos(data.items))
+  }, [searchTerm]);
+
   return (
-    <div>
-      Search
-    </div>
+    <Box p={2} sx={{ overflowY: 'auto', height: '90vh', flex: 2 }}>
+      <Typography variant="h4" fontWeight="bold" mb={2} sx={{color: 'white'}}>
+        Résultat pour : <span style={{color: '#F31503'}}>{searchTerm}</span>
+      </Typography>
+      <Video videos={videos}/>
+    </Box>
   )
 }
 
